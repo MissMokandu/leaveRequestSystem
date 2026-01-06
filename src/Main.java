331c868ecf8afe8eac56422257890f0060dc57f1
Benadette Mokandu <benadettemokandu@gmail.com>
2026-01-06 10:57:09 +0300

    employee class created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main{
-    public static void main (String[] args){
-        System.out.println("leave request system started");
+import model.Employee;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee emp = new Employee(1, "Benadette", "Employee");
+
+        System.out.println("Employee ID: " + emp.getId());
+        System.out.println("Employee Name: " + emp.getName());
+        System.out.println("Employee Role: " + emp.getRole());
     }
 }
